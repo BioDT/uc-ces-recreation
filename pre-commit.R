1d3152b56a268d0485defafcec4074c6a55403b8
jmarshrossney <17361029+jmarshrossney@users.noreply.github.com>
2025-03-25 10:21:45 +0000

    update pre-commit to work properly if run anywhere inside repo

--- pre-commit.R
+++ pre-commit.R
@@ -3,12 +3,18 @@ style <- styler::tidyverse_style(
     start_comments_with_one_space = TRUE
 )
 
-styler::style_dir("shiny_app", transformers = style, exclude_files = list("content.R", "theme.R"))
-lintr::lint_dir("shiny_app", exclusions = list("renv", "content.R", "theme.R"))
-styler::style_dir("cli", transformers = style)
-lintr::lint_dir("cli")
+git_root <- rprojroot::find_root(rprojroot::is_git_root)
 
-setwd("model")
+# Do the less important ones first!
+setwd(file.path(git_root, "cli"))
+styler::style_dir(transformers = style)
+lintr::lint_dir()
+
+setwd(file.path(git_root, "shiny_app"))
+styler::style_dir(transformers = style, exclude_files = list("content.R", "theme.R"))
+lintr::lint_dir(exclusions = list("renv", "content.R", "theme.R"))
+
+setwd(file.path(git_root, "model"))
 devtools::document()
 testthat::test_dir("tests/testthat")
 styler::style_pkg(transformers = style)
