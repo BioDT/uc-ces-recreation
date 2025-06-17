3eb20a82832f7f687fa30210fbe0c21f1917ae36
madtig <126075407+madtig@users.noreply.github.com>
2025-06-17 16:28:03 +0100

    adding tiling example (#92)
    * adding tiling example
    
    * adding the functions to data_production
    
    * temp_dir()
    
    * 500m data saved the rest NAs
    
    * run styler
    
    * update renv.lock
    
    * remove erroneously added symlink
    
    * undo changes to inst/scripts/data_production/main.R
    
    * move maddas test script to vignettes/drafts
    
    * rename scripts -> dev and move vignettes/drafts -> dev/drafts
    
    * exclude drafts from pre-commit hook (but already been linted, oops)
    
    * revert deletion of Bush example rasters
    
    * fix linting
    
    * remove yaml header from README.md
    
    ---------
    
    Co-authored-by: Maddalena Tigli <madtig@wlsc-biodt01.scicom.ceh.ac.uk>
    Co-authored-by: jmarshrossney <17361029+jmarshrossney@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,16 +1,15 @@
 # BioDT Recreation Potential model
 
-- Introduction to BioDT, appropriate links to UKCEH and BioDT
-- Recreational Potential is one half of the 'Cultural Ecosystem Services prototype Digital Twin' (CES pDT) developed by UKCEH.
-
+-   Introduction to BioDT, appropriate links to UKCEH and BioDT
+-   Recreational Potential is one half of the 'Cultural Ecosystem Services prototype Digital Twin' (CES pDT) developed by UKCEH.
 
 ## Overview
 
 ### Package
 
-This repository contains an implementation of the Recreational Potential model developed by \[CITATION\] as an `R` package.
+This repository contains an implementation of the Recreational Potential model developed by $$CITATION$$ as an `R` package.
 
-```R
+``` r
 > persona <- load_persona("path/to/personas.csv", name = "Running")
 > bbox <- terra::ext(xmin, xmax, ymin, ymax)  # must be within Scotland!
 > layers <- compute_potential(persona, bbox)
@@ -20,16 +19,16 @@ This repository contains an implementation of the Recreational Potential model d
 > plot(layers$Recreational_Potential)
 ```
 
-\[To do: add image\]
+$$To do: add image$$
 
 ### App
 
 The package comes bundled with an R Shiny app which enables users to visualise Recreational Potential values in Scotland, based on a customisable set of importance scores for 81 different items.
-This was developed independently of the [official BioDT app](https://app.biodt.eu/app/biodtshiny), and was used in a 2025 study _\[todo: links when complete\]_.
+This was developed independently of the [official BioDT app](https://app.biodt.eu/app/biodtshiny), and was used in a 2025 study $$todo: links when complete$$.
 
-\[To do: add image\]
+$$To do: add image$$
 
-A live instance of the Recreational Potential app is hosted at _\[todo: link to datalabs instance\]_.
+A live instance of the Recreational Potential app is hosted at $$todo: link to datalabs instance$$.
 
 ### Command-line interface
 
@@ -47,42 +46,40 @@ There is also a singularity container.
 
 Further details can be found in [inst/scripts/data_production/README.md](inst/scripts/data_production/README.md).
 
-
 ## For users
 
 ### Prerequisites
 
-- R version 4.4.x
-- Ensure you have either `remotes` or `devtools` installed (using e.g. `install.packages` or `renv::install`)
-- It is recommended to perform the following steps using an R envirnoment managed by [`renv`](https://rstudio.github.io/renv/).
-- A whole bunch of c++ libraries (documenting is a TODO - sorry!), most importantly related to GDAL
-
+-   R version 4.4.x
+-   Ensure you have either `remotes` or `devtools` installed (using e.g. `install.packages` or `renv::install`)
+-   It is recommended to perform the following steps using an R envirnoment managed by [`renv`](https://rstudio.github.io/renv/).
+-   A whole bunch of c++ libraries (documenting is a TODO - sorry!), most importantly related to GDAL
 
 ### Installation
 
-1. Install the package:
+1.  Install the package:
 
-```R
+``` r
 remotes::install_github("BioDT/uc-ces-recreation")
 ```
 
-2. Download the data:
+2.  Download the data:
 
-```R
+``` r
 biodt.recreation::download_data()
 ```
 
 ### Usage
 
-3. Run the app:
+3.  Run the app:
 
-```R
+``` r
 biodt.recreation::run_app()
 ```
 
-4. Use the package in a script
+4.  Use the package in a script
 
-```R
+``` r
 library(terra)
 library(biodt.recreation)
 
@@ -102,35 +99,34 @@ plot(layers$Recreational_Potential)
 
 Clone the repository
 
-```sh
+``` sh
 git clone https://github.com/BioDT/uc-ces-recreation
 cd uc-ces-recreation
 ```
 
 In an R session, install the dependencies
 
-```R
+``` r
 renv::restore()
 ```
 
-> [!NOTE]
-> If this does not work, try removing `renv.lock` and `renv/` and doing `renv::init()`, followed by selecting (1) 'explicit' mode, followed by (2) re-load library.
+> [!NOTE] If this does not work, try removing `renv.lock` and `renv/` and doing `renv::init()`, followed by selecting (1) 'explicit' mode, followed by (2) re-load library.
 
 Load the package (run this after making any changes!)
 
-```R
+``` r
 devtools::load_all()
 ```
 
 Download the data
 
-```R
+``` r
 download_data()
 ```
 
 Pull up the documentation for a function, e.g. `compute_potential`
 
-```R
+``` r
 ?biodt.recreation::compute_potential
 ```
 
@@ -145,7 +141,7 @@ R-specific instructions can be found at [lorenzwalthert.github.io/precommit](htt
 
 If you are happy for the `{precommit}` R package to handle everything, you can simply run the following in an R session in the repository root:
 
-```R
+``` r
 install.packages("precommit")
 precommit::install_precommit()  # omit this if you already installed pre-commit
 precommit::use_precommit()
@@ -156,14 +152,14 @@ Now, when you commit a bunch of hooks will run that will check various things.
 
 You can run the hooks manually using
 
-```sh
+``` sh
 pre-commit run --all-files
 ```
 
 ### Additional tools
 
-If you're comfortable running things from the shell, the `scripts/` directory may be useful to you. See [scripts/README.md](scripts/README.md) for further guidance.
-
+If you're comfortable running things from the shell, the `dev/` directory may be useful to you.
+See [dev/README.md](dev/README.md) for further guidance.
 
 ### Testing the installed package
 
@@ -171,55 +167,53 @@ It is a good idea to frequently test a fresh installation of the package, rather
 
 Create a fresh environment in a temporary directory
 
-```R
+``` r
 renv::init(bare = TRUE)
 renv::install("devtools")
 ```
 
 You can install from GitHub
 
-```R
+``` r
 remotes::install_github("BioDT/uc-ces-recreation")
 ```
 
 or locally
 
-```R
+``` r
 devtools::install("path/to/uc-ces-recreation", dependencies = TRUE)
 ```
 
-Download the data using 
+Download the data using
 
-```R
+``` r
 biodt.recreation::download_data()
 ```
 
 Run the tests
 
-```R
+``` r
 renv::install("testthat")
 testthat::test_package("biodt.recreation")
 ```
 
 Check the app works...
 
-```R
+``` r
 biodt.recreation::run_app()
 ```
-
 
 ### Contributing guidelines
 
 If you are interested in contributing, please take a quick look at [CONTRIBUTING.md](CONTRIBUTING.md).
 
-
 ## Contributors
 
-- Chris Andrews
-- Will Bolton
-- Joe Marsh Rossney @jmarshrossney
-- Simon Rolph
-- Maddalena Tigli
+-   Chris Andrews
+-   Will Bolton
+-   Joe Marsh Rossney @jmarshrossney
+-   Simon Rolph
+-   Maddalena Tigli
 
 ## Older versions
 
@@ -229,11 +223,12 @@ The code has gone through 3 major iterations.
 - 2024 version, primarily developed by Chris Andrews and Maddalena Tigli (https://github.com/BioDT/uc-ces-recreation/tree/2024-model)
 - 2025 version, primarily developed by Joe Marsh Rossney and Maddalena Tigli (this version)
 
+
 ## Acknowledgements
 
-- BioDT
-- SPEAK funding, and feedback from participants in this study
+-   BioDT
+-   SPEAK funding, and feedback from participants in this study
 
 ## Citation
 
-\[TODO\]
+$$TODO$$
---
+++ dev/drafts/MT_test_split.R
@@ -0,0 +1,229 @@
+#############################################
+####### split the raster in n sections ######
+# returns a list of tiles
+#############################################
+
+split_raster_with_buffer <- function(r, target_tiles, buffer_dist) {
+    # 1) check format
+    if (!inherits(r, "SpatRaster")) {
+        stop("Input must be a 'SpatRaster' object from the terra package.")
+    }
+
+    # 2) split in tiles
+
+    nrow_ras <- nrow(r)
+    ncol_ras <- ncol(r)
+
+    aspect <- ncol_ras / nrow_ras
+    ncols <- ceiling(sqrt(target_tiles * aspect))
+    nrows <- ceiling(target_tiles / ncols)
+
+    message("Will split into ", nrows * ncols, " tiles (", nrows, " rows × ", ncols, " cols)")
+
+    e <- terra::ext(r)
+    tile_width <- (e[2] - e[1]) / ncols
+    tile_height <- (e[4] - e[3]) / nrows
+
+    tiles <- vector("list", length = ncols * nrows)
+    k <- 1
+    for (i in 0:(ncols - 1)) {
+        for (j in 0:(nrows - 1)) {
+            xmin <- e[1] + i * tile_width
+            xmax <- xmin + tile_width
+            ymin <- e[3] + j * tile_height
+            ymax <- ymin + tile_height
+            orig_extent <- terra::ext(xmin, xmax, ymin, ymax)
+
+            # Edge checks
+            on_left <- i == 0
+            on_right <- i == (ncols - 1)
+            on_bottom <- j == 0
+            on_top <- j == (nrows - 1)
+
+            xmin_buf <- if (!on_left) xmin - buffer_dist else xmin
+            xmax_buf <- if (!on_right) xmax + buffer_dist else xmax
+            ymin_buf <- if (!on_bottom) ymin - buffer_dist else ymin
+            ymax_buf <- if (!on_top) ymax + buffer_dist else ymax
+
+            buf_extent <- terra::ext(xmin_buf, xmax_buf, ymin_buf, ymax_buf)
+
+            # Store both extents in a named list
+            tiles[[k]] <- list(
+                buffer = buf_extent,
+                original = orig_extent
+            )
+            k <- k + 1
+        }
+    }
+
+    # end
+    return(tiles)
+}
+
+
+
+#############################################
+####### re-crop to original tiles size ######
+# returns cropped tile
+#############################################
+crop_to_original_extent <- function(raster, tile_info) {
+    # check format
+    if (!inherits(raster, "SpatRaster")) {
+        stop("'raster' must be a SpatRaster")
+    }
+
+    if (!is.list(tile_info) || is.null(tile_info$original)) {
+        stop("'tile_info' must be a list with an 'original' element containing an extent.")
+    }
+
+    return(terra::crop(raster, tile_info$original))
+}
+
+
+
+##############################################
+######### test splitting  #############
+##############################################
+test_raster_path <- "~/Desktop/repositories/uc-ces-recreation2/inst/extdata/rasters/Stage_3/Water.tif"
+# test_raster_path <-"~/Desktop/repositories/uc-ces-recreation2/inst/extdata/rasters/Bush/Water.tif"
+
+r <- terra::rast(test_raster_path)
+
+buffer_dist <- 10000 # in m
+
+tiles <- split_raster_with_buffer(r = r, target_tiles = 20, buffer_dist = buffer_dist) # target tiles is approximate as it must be able to divide them equally etc..
+
+
+###### calculate distance for each chunck
+
+output_files <- character(length(tiles))
+original_layer_names <- names(r)
+
+for (i in seq_along(tiles)) {
+    message("Processing tile ", i, "/", length(tiles))
+
+    tile_info <- tiles[[i]]
+    chunk <- terra::crop(r, tile_info$buffer)
+
+    # Initialize list to hold processed distance layers
+    dist_layers <- vector("list", length = length(original_layer_names))
+    names(dist_layers) <- original_layer_names
+
+    for (layer_idx in seq_along(original_layer_names)) {
+        layer_name <- original_layer_names[layer_idx]
+
+        if (layer_name %in% names(chunk)) {
+            lyr <- chunk[[layer_name]]
+
+            has_values <- terra::global(lyr, fun = function(x) any(!is.na(x)))[[1]]
+
+            if (has_values) {
+                # Compute distance for this layer
+                dist_layer <- terra::distance(lyr, target = NA, unit = "m", method = "haversine")
+                dist_layers[[layer_name]] <- dist_layer
+            } else {
+                # Replace with empty raster matching layer extent and resolution
+                empty_ras <- terra::rast(lyr)
+                terra::values(empty_ras) <- NA
+                dist_layers[[layer_name]] <- empty_ras
+            }
+        } else {
+            # Layer not present (e.g. cropped out), use empty raster
+            lyr_template <- terra::crop(r[[layer_name]], tile_info$buffer)
+            terra::values(lyr_template) <- NA
+            dist_layers[[layer_name]] <- lyr_template
+        }
+    }
+
+    # Stack layers back into a SpatRaster
+    dist_ras <- terra::rast(dist_layers)
+    names(dist_ras) <- original_layer_names
+
+    # Crop back to original (non-buffered) extent
+    dist_ras_cropped <- crop_to_original_extent(dist_ras, tile_info)
+
+    # Save to file
+    outfile <- paste0("tile_dist_", i, ".tif")
+    if (file.exists(outfile)) file.remove(outfile)
+    terra::writeRaster(dist_ras_cropped, outfile, overwrite = TRUE)
+    output_files[i] <- outfile
+}
+
+##### re adding them
+message("re-adding them all", i, "/", length(tiles))
+rasters <- lapply(output_files, terra::rast)
+full_distance_raster <- do.call(terra::mosaic, rasters)
+names(full_distance_raster) <- original_layer_names
+
+terra::writeRaster(full_distance_raster, "full_distance_result.tif", overwrite = TRUE)
+
+file.remove(output_files)
+
+# show <- terra::rast("full_distance_result_bush.tif")
+# terra::plot(show)
+
+
+
+################ test with original function ############
+compute_distance <- function(infile, outfile) {
+    raster <- terra::rast(infile)
+
+    ##### check if there are "empty layers" in the selected area - if so, exclude them
+    empty_layers <- c()
+    for (i in 1:terra::nlyr(raster)) {
+        # stops as soon as it find 1 value that is not NA and moves on
+        non_na_found <- terra::global(raster[[i]], fun = function(x) any(!is.na(x)))[[1]]
+        if (!non_na_found) {
+            empty_layers <- c(empty_layers, names(raster)[i])
+        }
+    }
+    if (length(empty_layers) > 0) {
+        print(paste("excluding layers", empty_layers, "because they seem empty"))
+        raster <- raster[[!names(raster) %in% empty_layers]]
+    }
+
+    # ##### check if there are "completely full" layers, if so, they all need to be 1
+    # full_layers <- c()
+    # for (i in 1:terra::nlyr(raster)) {
+    # # Check if all values are NOT NA (i.e., fully populated layer)
+    # all_non_na <- terra::global(raster[[i]], fun = function(x) all(!is.na(x)))[[1]]
+    # if (all_non_na) {
+    # full_layers <- c(full_layers, names(raster)[i])
+    # }
+    # }
+    # if (length(full_layers) > 0) {
+    # print(paste("excluding layers", full_layers, "because they are 100% full (no NAs)"))
+    # raster <- raster[[!names(raster) %in% full_layers]]
+    # }
+
+    ## the layers that have both Nas and Numbers can have the distance calculated
+    # terra::distance takes every single NA and assigns that cell a value that's the distance to the nearest 1.
+    # The cells that are 1 get assigned 0.
+
+    # check_layers <- function(r) {
+    # sapply(1:terra::nlyr(r), function(i) {
+    # lyr <- r[[i]]
+    # v <- terra::values(lyr, mat = FALSE)
+    # all(is.na(v) | v == 1)
+    # })
+    # }
+    # all_layers_valid <- all(check_layers(raster))
+
+    terra::distance(
+        raster,
+        target = NA, # targets everything excluding the features (v expensive!)
+        unit = "m",
+        method = "haversine",
+        filename = outfile,
+        datatype = "FLT4S",
+        overwrite = TRUE
+    )
+}
+
+# infile <- test_raster_path
+# outfile <- "full_distance_check.tif"
+# compute_distance(infile = infile,
+# outfile= outfile)
+
+# show_check <- terra::rast("full_distance_check.tif")
+# terra::plot(show_check)
