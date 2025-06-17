7080114f89ec59de18e94da4ac4cd1014bd1e177
jmarshrossney <17361029+jmarshrossney@users.noreply.github.com>
2025-06-17 09:16:57 +0100

    run styler

--- R/data_production.R
+++ R/data_production.R
@@ -256,19 +256,19 @@ stack_all <- function(indir, outdir) {
 #' @export
 compute_distance <- function(infile, outfile) {
     raster <- terra::rast(infile)
-    
+
     ##### check if there are "empty layers" in the selected area - if so, exclude them
     empty_layers <- c()
     for (i in 1:terra::nlyr(raster)) {
-      #stops as soon as it find 1 value that is not NA and moves on
-      non_na_found <- terra::global(raster[[i]], fun = function(x) any(!is.na(x)))[[1]]
-      if (!non_na_found) {
-        empty_layers <- c(empty_layers, names(raster)[i])
-      }
+        # stops as soon as it find 1 value that is not NA and moves on
+        non_na_found <- terra::global(raster[[i]], fun = function(x) any(!is.na(x)))[[1]]
+        if (!non_na_found) {
+            empty_layers <- c(empty_layers, names(raster)[i])
+        }
     }
-    if (length(empty_layers) > 0){
-      print(paste("excluding layers", empty_layers, "because they seem empty"))
-      raster <- raster[[!names(raster) %in% empty_layers]]
+    if (length(empty_layers) > 0) {
+        print(paste("excluding layers", empty_layers, "because they seem empty"))
+        raster <- raster[[!names(raster) %in% empty_layers]]
     }
 
     terra::distance(
@@ -280,7 +280,6 @@ compute_distance <- function(infile, outfile) {
         datatype = "FLT4S",
         overwrite = TRUE
     )
-    
 }
 
 # TODO: add link to paper, equation, values for kappa & alpha
@@ -303,10 +302,10 @@ map_distance_to_unit <- function(infile, outfile, kappa = 6, alpha = 0.01011) {
         (kappa + 1) / (kappa + exp(alpha * x))
     }
     raster <- terra::rast(infile)
-    
-    #exclude cells that are further than 500m from features, as this won't impact the model output and setting values to NAs does reduce file size when compressed
+
+    # exclude cells that are further than 500m from features, as this won't impact the model output and setting values to NAs does reduce file size when compressed
     raster[raster > 500] <- NA
-    
+
     terra::app(
         raster,
         fun = logistic_func,
@@ -342,50 +341,49 @@ compute_proximity_rasters <- function(indir, outdir, splitting) {
         infile <- file.path(indir, paste0(component, ".tif"))
         dist_file <- file.path(outdir, paste0(component, "_dist.tif"))
         outfile <- file.path(outdir, paste0(component, ".tif"))
-        
-        #check it wasn't already done 
+
+        # check it wasn't already done
         if (file.exists(dist_file) && file.exists(outfile)) {
-          cat("Files for", component, "already exist. Skipping...\n")
-          next  # Skip this iteration
+            cat("Files for", component, "already exist. Skipping...\n")
+            next # Skip this iteration
         }
-        
+
         dir.create(dirname(outfile), recursive = TRUE, showWarnings = FALSE)
 
-        if(!file.exists(dist_file)){
-          message(paste("Performing distance calculation:", infile, "->", dist_file))
-          if (splitting == T){
-            timed(compute_distance_window)(infile, dist_file, target_tiles = 20, buffer_dist = 10000)
-          }else{
-            timed(compute_distance)(infile, dist_file)
-          }
-          message(paste("Mapping distance to unit interval:", dist_file, "->", outfile))
-          timed(map_distance_to_unit)(dist_file, outfile)
-        }else {
-          message(paste("distance file found, only calculating the map to distance"))
-          message(paste("Mapping distance to unit interval:", dist_file, "->", outfile))
-          timed(map_distance_to_unit)(dist_file, outfile)
+        if (!file.exists(dist_file)) {
+            message(paste("Performing distance calculation:", infile, "->", dist_file))
+            if (splitting == T) {
+                timed(compute_distance_window)(infile, dist_file, target_tiles = 20, buffer_dist = 10000)
+            } else {
+                timed(compute_distance)(infile, dist_file)
+            }
+            message(paste("Mapping distance to unit interval:", dist_file, "->", outfile))
+            timed(map_distance_to_unit)(dist_file, outfile)
+        } else {
+            message(paste("distance file found, only calculating the map to distance"))
+            message(paste("Mapping distance to unit interval:", dist_file, "->", outfile))
+            timed(map_distance_to_unit)(dist_file, outfile)
         }
-        
     }
-  
-  
-  
+
+
+
     for (component in c("SLSRA", "FIPS_N")) {
         infile <- file.path(indir, component)
         outfile <- file.path(outdir, paste0(component, ".tif"))
         dir.create(dirname(outfile), recursive = TRUE, showWarnings = FALSE)
-        
+
         message(paste("Creating symbolic link:", infile, "->", outfile))
-        if(file.exists(outfile)){
-          file.remove(outfile)
+        if (file.exists(outfile)) {
+            file.remove(outfile)
         }
         file.symlink(infile, outfile)
     }
-  print("DONE compute_proximity_rasters")
+    print("DONE compute_proximity_rasters")
 }
 
 #' create_overlapping_windows
-#' 
+#'
 #' For a raster provided, split into n windows and their buffer of n meters (to maintain distance calculation relevant).
 #'
 #' @param r a Spatrast element read in using terra::rast(raster_name.tif)
@@ -395,66 +393,65 @@ compute_proximity_rasters <- function(indir, outdir, splitting) {
 #' @keywords internal
 #' @export
 create_overlapping_windows <- function(r, target_tiles, buffer_dist) {
-  
-  # 1) check format
-  if (!inherits(r, "SpatRaster")) {
-    stop("Input must be a 'SpatRaster' object from the terra package.")
-  }
-  
-  # 2) split in tiles
-  #how many tiles
-  nrow_ras <- nrow(r)
-  ncol_ras <- ncol(r)
-  
-  aspect <- ncol_ras / nrow_ras
-  ncols <- ceiling(sqrt(target_tiles * aspect))
-  nrows <- ceiling(target_tiles / ncols)
-  
-  message("Will split into ", nrows * ncols, " tiles (", nrows, " rows × ", ncols, " cols)")
-  
-  #split them
-  e <- terra::ext(r)
-  tile_width  <- (e[2] - e[1]) / ncols
-  tile_height <- (e[4] - e[3]) / nrows
-  
-  tiles <- vector("list", length = ncols * nrows)
-  k <- 1
-  for (i in 0:(ncols - 1)) {
-    for (j in 0:(nrows - 1)) {
-      xmin <- e[1] + i * tile_width
-      xmax <- xmin + tile_width
-      ymin <- e[3] + j * tile_height
-      ymax <- ymin + tile_height
-      orig_extent <- terra::ext(xmin, xmax, ymin, ymax)
-      
-      # Edge checks
-      on_left   <- i == 0
-      on_right  <- i == (ncols - 1)
-      on_bottom <- j == 0
-      on_top    <- j == (nrows - 1)
-      
-      xmin_buf <- if (!on_left)  xmin - buffer_dist else xmin
-      xmax_buf <- if (!on_right) xmax + buffer_dist else xmax
-      ymin_buf <- if (!on_bottom) ymin - buffer_dist else ymin
-      ymax_buf <- if (!on_top)    ymax + buffer_dist else ymax
-      
-      buf_extent <- terra::ext(xmin_buf, xmax_buf, ymin_buf, ymax_buf)
-      
-      # Store both extents in a named list
-      tiles[[k]] <- list(
-        buffer = buf_extent,
-        original = orig_extent
-      )
-      k <- k + 1
+    # 1) check format
+    if (!inherits(r, "SpatRaster")) {
+        stop("Input must be a 'SpatRaster' object from the terra package.")
     }
-  }
-  
-  #end
-  return(tiles)
+
+    # 2) split in tiles
+    # how many tiles
+    nrow_ras <- nrow(r)
+    ncol_ras <- ncol(r)
+
+    aspect <- ncol_ras / nrow_ras
+    ncols <- ceiling(sqrt(target_tiles * aspect))
+    nrows <- ceiling(target_tiles / ncols)
+
+    message("Will split into ", nrows * ncols, " tiles (", nrows, " rows × ", ncols, " cols)")
+
+    # split them
+    e <- terra::ext(r)
+    tile_width <- (e[2] - e[1]) / ncols
+    tile_height <- (e[4] - e[3]) / nrows
+
+    tiles <- vector("list", length = ncols * nrows)
+    k <- 1
+    for (i in 0:(ncols - 1)) {
+        for (j in 0:(nrows - 1)) {
+            xmin <- e[1] + i * tile_width
+            xmax <- xmin + tile_width
+            ymin <- e[3] + j * tile_height
+            ymax <- ymin + tile_height
+            orig_extent <- terra::ext(xmin, xmax, ymin, ymax)
+
+            # Edge checks
+            on_left <- i == 0
+            on_right <- i == (ncols - 1)
+            on_bottom <- j == 0
+            on_top <- j == (nrows - 1)
+
+            xmin_buf <- if (!on_left) xmin - buffer_dist else xmin
+            xmax_buf <- if (!on_right) xmax + buffer_dist else xmax
+            ymin_buf <- if (!on_bottom) ymin - buffer_dist else ymin
+            ymax_buf <- if (!on_top) ymax + buffer_dist else ymax
+
+            buf_extent <- terra::ext(xmin_buf, xmax_buf, ymin_buf, ymax_buf)
+
+            # Store both extents in a named list
+            tiles[[k]] <- list(
+                buffer = buf_extent,
+                original = orig_extent
+            )
+            k <- k + 1
+        }
+    }
+
+    # end
+    return(tiles)
 }
 
 #' crop_to_original_extent
-#' 
+#'
 #' For a window provided, use the output of [biodt.recreation::create_overlapping_windows] to re-crop the raster to the un-buffered window extent.
 #' **Relies on output of:** [biodt.recreation::create_overlapping_windows]
 #' **Preceded by:** [biodt.recreation::compute_distance]
@@ -464,20 +461,20 @@ create_overlapping_windows <- function(r, target_tiles, buffer_dist) {
 #' @keywords internal
 #' @export
 crop_to_original_extent <- function(raster, tile_info) {
-  #1) check format
-  if (!inherits(raster, "SpatRaster")) {
-    stop("'raster' must be a SpatRaster")
-  }
-  #2) check list of extents available
-  if (!is.list(tile_info) || is.null(tile_info$original)) {
-    stop("'tile_info' must be a list with an 'original' element containing an extent.")
-  }
-  #3) crop to window
-  return(terra::crop(raster, tile_info$original))
+    # 1) check format
+    if (!inherits(raster, "SpatRaster")) {
+        stop("'raster' must be a SpatRaster")
+    }
+    # 2) check list of extents available
+    if (!is.list(tile_info) || is.null(tile_info$original)) {
+        stop("'tile_info' must be a list with an 'original' element containing an extent.")
+    }
+    # 3) crop to window
+    return(terra::crop(raster, tile_info$original))
 }
 
 #' compute_distance_window
-#' 
+#'
 #' Given a spatrast, it splits it into windows using [biodt.recreation::create_overlapping_windows], computes the distance to the nearest non-`NA` value in a raster
 #' using haversine distance, writing straight to `outfile`.Recomposes the main raster using the windows using
 #' [biodt.recreation::crop_to_original_extent] and, saves the output and deletes intermediate files.
@@ -489,75 +486,72 @@ crop_to_original_extent <- function(raster, tile_info) {
 #'
 #' @keywords internal
 #' @export
-compute_distance_window <- function (infile, dist_file, target_tiles = 20, buffer_dist = 10000){
-  
-  #1) read the raster
-  r <- terra::rast(infile)
-  #2) calculate the window
-  tiles <- create_overlapping_windows(r = r, target_tiles = target_tiles, buffer_dist = buffer_dist)
-  
-  #2b) create temporary directory to store files in
-  temp_dir <- file.path(tempdir(), paste0("distance_tiles_", as.integer(Sys.time())))
-  dir.create(temp_dir, recursive = TRUE)
-  message("Temporary files will be saved in: ", temp_dir)
-  
-  #3) calculate distance for each window
-  output_files <- character(length(tiles))
-  original_layer_names <- names(r)
-  
-  for (i in seq_along(tiles)) {
-    
-    message("Processing tile ", i, "/", length(tiles))
-    
-    tile_info <- tiles[[i]]
-    chunk <- terra::crop(r, tile_info$buffer)
-    
-    #list to hold processed distance layers
-    dist_layers <- vector("list", length = length(original_layer_names))
-    names(dist_layers) <- original_layer_names
-    
-    #4)for each layer in the window check it has values, if it doesn't skip the distance calculation
-    for (layer_idx in seq_along(original_layer_names)) {
-      layer_name <- original_layer_names[layer_idx]
-      lyr <- chunk[[layer_name]]
-      has_values <- terra::global(lyr, fun = function(x) any(!is.na(x)))[[1]]
-      
-      if (has_values) {
-        # 5) Compute the distance for the layer in the window
-        dist_layer <- terra::distance(lyr, target = NA, unit = "m", method = "haversine")
-        dist_layers[[layer_name]] <- dist_layer
-      } else {
-        # (if layer all NAs, skip it) (a.k.a. use empty raster)
-        lyr_template <- terra::crop(r[[layer_name]], tile_info$buffer)
-        terra::values(lyr_template) <- NA
-        dist_layers[[layer_name]] <- lyr_template
-      }
+compute_distance_window <- function(infile, dist_file, target_tiles = 20, buffer_dist = 10000) {
+    # 1) read the raster
+    r <- terra::rast(infile)
+    # 2) calculate the window
+    tiles <- create_overlapping_windows(r = r, target_tiles = target_tiles, buffer_dist = buffer_dist)
+
+    # 2b) create temporary directory to store files in
+    temp_dir <- file.path(tempdir(), paste0("distance_tiles_", as.integer(Sys.time())))
+    dir.create(temp_dir, recursive = TRUE)
+    message("Temporary files will be saved in: ", temp_dir)
+
+    # 3) calculate distance for each window
+    output_files <- character(length(tiles))
+    original_layer_names <- names(r)
+
+    for (i in seq_along(tiles)) {
+        message("Processing tile ", i, "/", length(tiles))
+
+        tile_info <- tiles[[i]]
+        chunk <- terra::crop(r, tile_info$buffer)
+
+        # list to hold processed distance layers
+        dist_layers <- vector("list", length = length(original_layer_names))
+        names(dist_layers) <- original_layer_names
+
+        # 4)for each layer in the window check it has values, if it doesn't skip the distance calculation
+        for (layer_idx in seq_along(original_layer_names)) {
+            layer_name <- original_layer_names[layer_idx]
+            lyr <- chunk[[layer_name]]
+            has_values <- terra::global(lyr, fun = function(x) any(!is.na(x)))[[1]]
+
+            if (has_values) {
+                # 5) Compute the distance for the layer in the window
+                dist_layer <- terra::distance(lyr, target = NA, unit = "m", method = "haversine")
+                dist_layers[[layer_name]] <- dist_layer
+            } else {
+                # (if layer all NAs, skip it) (a.k.a. use empty raster)
+                lyr_template <- terra::crop(r[[layer_name]], tile_info$buffer)
+                terra::values(lyr_template) <- NA
+                dist_layers[[layer_name]] <- lyr_template
+            }
+        }
+
+        # Stack layers back into a SpatRaster
+        dist_ras <- terra::rast(dist_layers)
+        names(dist_ras) <- original_layer_names
+
+        # Crop back to original (non-buffered) extent
+        dist_ras_cropped <- crop_to_original_extent(dist_ras, tile_info)
+
+        # Save to file
+        outfile <- file.path(temp_dir, paste0("tile_dist_", i, ".tif"))
+        if (file.exists(outfile)) file.remove(outfile)
+        terra::writeRaster(dist_ras_cropped, outfile, overwrite = TRUE)
+        output_files[i] <- outfile
     }
-    
-    # Stack layers back into a SpatRaster
-    dist_ras <- terra::rast(dist_layers)
-    names(dist_ras) <- original_layer_names
-    
-    # Crop back to original (non-buffered) extent
-    dist_ras_cropped <- crop_to_original_extent(dist_ras, tile_info)
-    
-    # Save to file
-    outfile <- file.path(temp_dir, paste0("tile_dist_", i, ".tif"))
-    if (file.exists(outfile)) file.remove(outfile)
-    terra::writeRaster(dist_ras_cropped, outfile, overwrite = TRUE)
-    output_files[i] <- outfile
-  }
-  
-  #6) recomposing the one master raster from the windows
-  message("re-adding them all", i, "/", length(tiles))
-  rasters <- lapply(output_files, terra::rast)
-  full_distance_raster <- do.call(terra::mosaic, rasters)
-  names(full_distance_raster) <- original_layer_names
-  
-  #7) writing the final distance raster
-  terra::writeRaster(full_distance_raster, dist_file, overwrite = TRUE)
-  
-  # Delete the entire temp directory with all intermediate files
-  unlink(temp_dir, recursive = TRUE)
-  
+
+    # 6) recomposing the one master raster from the windows
+    message("re-adding them all", i, "/", length(tiles))
+    rasters <- lapply(output_files, terra::rast)
+    full_distance_raster <- do.call(terra::mosaic, rasters)
+    names(full_distance_raster) <- original_layer_names
+
+    # 7) writing the final distance raster
+    terra::writeRaster(full_distance_raster, dist_file, overwrite = TRUE)
+
+    # Delete the entire temp directory with all intermediate files
+    unlink(temp_dir, recursive = TRUE)
 }
